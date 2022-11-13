8d37a43431d153471631dc774802ba147f4e8db2
JrodMc <jmcguire72@ivytech.edu>
2022-11-12 20:07:57 -0500

    Files for M03 Node.js tutorial.

---
+++ node-crash-course/modules.js
@@ -0,0 +1,11 @@
+// const people = require('./people');
+
+// console.log(people);
+
+const { people, ages } = require('./people');
+
+console.log(people, ages);
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
